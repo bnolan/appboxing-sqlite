6e5bc6d440f82bc83071a8d57ece117f13384665
Ben Nolan <bnolan@gmail.com>
2016-11-26 11:47:58 +1300

    Initial commit.

---
+++ index.js
@@ -0,0 +1,59 @@
+const server = require('http').createServer();
+const url = require('url');
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({ server: server });
+const express = require('express');
+const app = express();
+const port = 4100;
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+app.use(function (req, res) {
+  res.send({ msg: 'hello' });
+});
+ 
+wss.on('connection', (ws) => {
+  var location = url.parse(ws.upgradeReq.url, true);
+  // you might use location.query.access_token to authenticate or share sessions 
+  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312) 
+
+  ws.on('message', (message) => {
+    console.log('received: %s', message);
+
+    const packet = JSON.parse(message);
+    var callback = () => {};
+
+    if (packet.callback === true) {
+      // nb: Callback can trigger multiple times (in an each for example)
+      callback = (err, row) => {
+        ws.send(JSON.stringify({
+          id: packet.id,
+          err: err,
+          row: row
+        }));
+      };
+    }
+
+    if (packet.command === 'run') {
+      const query = packet.query;
+
+      if (typeof query === 'string') {
+        db.run(query, [], callback);
+      } else {
+        db.run(query.query, query.arguments, callback);
+      }
+    }
+
+    if (packet.command === 'each') {
+      db.each(packet.query, [], callback);
+    }
+  });
+
+  ws.send('something');
+});
+
+server.on('request', app);
+server.listen(port, () => { 
+  console.log('Listening on ' + server.address().port); 
+});
