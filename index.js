94e6c1b0c643d983149b430d573e4b1d6974f7ee
Ben Nolan <bnolan@gmail.com>
2016-12-03 10:20:18 +1300

    onConnected.

--- index.js
+++ index.js
@@ -1,65 +1,93 @@
 const server = require('http').createServer();
 const url = require('url');
+const path = require('path');
 const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ server: server });
 const express = require('express');
 const app = express();
 const port = 4100;
 
+const assert = require('assert');
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
 
 app.use(function (req, res) {
   res.send({ msg: 'hello' });
 });
  
 wss.on('connection', (ws) => {
-  var location = url.parse(ws.upgradeReq.url, true);
-  // you might use location.query.access_token to authenticate or share sessions 
-  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312) 
+  // var location = url.parse(ws.upgradeReq.url, true);
+  // you might use location.query.access_token to authenticate or share sessions
+  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
 
   // todo - send hello message?
 
-  db.serialize(() => {
-    ws.on('message', (message) => {
-      console.log('received: %s', message);
+  var db = null;
+  var databaseConnected = false;
+  var queue = [];
 
-      const packet = JSON.parse(message);
-      var callback = () => {};
+  ws.on('message', (message) => {
+    console.log('received: %s', message);
+    const packet = JSON.parse(message);
 
-      if (packet.callback === true) {
-        // nb: Callback can trigger multiple times (in an each for example)
-        callback = (err, row) => {
-          ws.send(JSON.stringify({
-            id: packet.id,
-            err: err,
-            row: row
-          }));
-        };
-      }
+    if (packet.command === 'hello') {
+      // todo parse and check many many things
+      db = new sqlite3.Database(path.join(__dirname, '..', packet.app.name + '.sqlite'), onConnected);
+      // db = new sqlite3.Database(':memory:');
 
-      const query = packet.query;
+      return;
+    }
 
-      if (packet.command === 'run') {
-        if (typeof query === 'string') {
-          db.run(query, [], callback);
-        } else {
-          db.run(query.query, query.arguments, callback);
-        }
-      }
-
-      if (packet.command === 'each') {
-        if (typeof query === 'string') {
-          db.each(query, [], callback);
-        } else {
-          db.each(query.query, query.arguments, callback);
-        }
-      }
-    });
+    if (databaseConnected) {
+      processPacket(packet);
+    } else {
+      assert(queue);
+      queue.push(packet);
+    }
   });
+
+  function onConnected () {
+    databaseConnected = true;
+
+    queue.forEach((packet) => {
+      processPacket(packet);
+    });
+
+    queue = null;
+
+    // We always force this
+    db.serialize();
+  }
+
+  function processPacket (packet) {
+    var callback = () => {};
+
+    if (packet.callback === true) {
+      // nb: Callback can trigger multiple times (in an each for example)
+      callback = (err, row) => {
+        ws.send(JSON.stringify({
+          id: packet.id,
+          err: err,
+          row: row
+        }));
+      };
+    }
+
+    const query = packet.query;
+
+    assert(typeof query === 'object');
+
+    if (packet.command === 'run') {
+      db.run(query.query, query.arguments, callback);
+    }
+
+    if (packet.command === 'each') {
+      db.each(query.query, query.arguments, callback);
+    }
+  }
 });
 
 server.on('request', app);
+
 server.listen(port, () => { 
   console.log('Listening on ' + server.address().port); 
 });
